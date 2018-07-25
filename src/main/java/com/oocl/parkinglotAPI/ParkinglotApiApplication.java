39bd9771875965417e7be7e1b0d93606f1efa725
TRACY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTR2@OOCL.COM>
2018-07-25 13:54:14 +0800

    init

---
+++ src/main/java/com/oocl/parkinglotAPI/ParkinglotApiApplication.java
@@ -0,0 +1,12 @@
+package com.oocl.parkinglotAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinglotApiApplication.class, args);
+	}
+}
